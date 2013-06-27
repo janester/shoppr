52750b4a4a3d0a005044681d3d5ce393ff96cac2
Jane Sternbach <janesternbach@gmail.com>
2013-06-27 16:16:28 -0700

    began loop of results

--- shoppr.rb
+++ shoppr.rb
@@ -1,2 +1,8 @@
 require "pry"
 require "httparty"
+
+results = HTTParty.get("https://api.gilt.com/v1/products?q=jeans&store=women&apikey=363a9cbd2888c4a7a25645f56ffa20a0")
+def parse_gilt_results(results)
+  results["products"].each do |product|
+  end
+end
